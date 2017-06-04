1b12c59076f157d761b91a63859247e4c7768a0f
klavdij <klavdij.starman1@gmail.com>
2017-06-04 14:11:31 +0200

    initial commit

---
+++ app/src/main/java/com/example/klavdij/carfinder/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.klavdij.carfinder;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SwipePlaceHolderView mSwipeView;
+    private Context mContext;
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSwipeView = (SwipePlaceHolderView)findViewById(R.id.swipeView);
+        mContext = getApplicationContext();
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
+
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(3)
+                .setSwipeDecor(new SwipeDecor()
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.swipe_in_msg_view)
+                        .setSwipeOutMsgLayoutId(R.layout.swipe_out_msg_view));
+
+        for(Profile profile : Utils.loadProfiles(this.getApplicationContext())){
+            mSwipeView.addView(new TinderCard(mContext, profile, mSwipeView));
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // handle desired action here
+                // One possibility of action is to replace the contents above the nav bar
+                // return true if you want the item to be displayed as the selected item
+                return true;
+            }
+        });
+    }
+}
